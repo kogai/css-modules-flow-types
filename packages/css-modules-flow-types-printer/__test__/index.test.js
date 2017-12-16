a9c23382c4744f12f2d1ecdd2f3461fc571ae379
shinchi kogai <kogai0121@gmail.com>
2017-12-16 18:13:01 +0900

    Export reason interface file

--- packages/css-modules-flow-types-printer/__test__/index.test.js
+++ packages/css-modules-flow-types-printer/__test__/index.test.js
@@ -1,15 +1,25 @@
 /* eslint-env jest */
-import printer, { HEADER } from '../index';
+import { HEADER, reason, flow } from '../index';
 
 describe('printer', () => {
-  it('prints given tokens', () => {
-    const result = printer(['btn1', 'btn2']);
+  it('prints given tokens to flow def', () => {
+    const result = flow(['btn1', 'btn2']);
     expect(result).toBe(
       `${HEADER}
 declare module.exports: {|
   +'btn1': string;
   +'btn2': string;
 |};
+`
+    );
+  });
+  it('prints given tokens to reason interface', () => {
+    const result = reason('myStyle.css', ['btn1', 'btn2']);
+    expect(result).toBe(
+      `[@bs.module] external style : Js.t({
+  'btn1': string,
+  'btn2': string
+}) = "./myStyle.css";
 `
     );
   });
